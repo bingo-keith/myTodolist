2917ff6a82093f404aa14f410cc5955d779b768c
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2018-08-01 17:12:19 +0800

    my second push

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
 //在顶层创建store管理整个项目的数据
-const store = createStore(todoApp)
+const store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 console.log('9. root index start')
 //connect方法生成容器组件以后，需要让容器组件拿到state对象，才能生成 UI 组件的参数。
 // 一种解决方法是将state对象作为参数，传入容器组件。但是，这样做比较麻烦，尤其是容器组件可能在很深的层级，一级级将state传下去就很麻烦。
